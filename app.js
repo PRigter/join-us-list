9287c01f62b3232517823e9d1957636cb871f40a
Paulo Rigter <paulorigter@gmail.com>
2020-01-12 21:56:21 +0000

    testing

--- app.js
+++ app.js
@@ -24,18 +24,19 @@ app.get("/", async (req, res) => {
 	// Find count of users in DB
 	var q = "SELECT COUNT(*) AS count FROM users"
 	
-	// connection.query(q, function(error, results) {
-	// 	if(error) throw error
-	// 	var count = results[0].count
-	// 	console.log(count)
+	connection.query(q, function(error, results) {
+		if(error) throw error
 		
-	// // 	// res.send("We have " + count + " users in our DB!")
-	// 	res.render("home", {count: count})
+		var count = results[0].count
+		// console.log(count)
 		
-	//  })
+	// 	// res.send("We have " + count + " users in our DB!")
+		res.render("home", {count: count})
+		
+	 })
 	
 
-	res.render("home")
+
 
 })
 
